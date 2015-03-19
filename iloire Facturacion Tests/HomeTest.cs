9697107963a696cecdcf985f2df453021d43158f
Penn Dai <penndai@hotmail.com>
2015-03-19 15:29:13 +1100

    initially commint on 19/03/2015

---
+++ iloire Facturacion Tests/HomeTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using CoffeeInvoice.Controllers;
+
+namespace CoffeeInvoiceTests
+{
+    [TestFixture]
+    public class HomeTest
+    {
+        [TestFixtureSetUp]
+        public void TestSetup()
+        {
+         
+        }
+
+        [Test]
+        public void TestHomeIndex()
+        {
+            HomeController hc = new HomeController();
+            System.Web.Mvc.ActionResult result = hc.Index();
+            Assert.IsInstanceOf(typeof(System.Web.Mvc.ViewResult), result);
+        }
+    }
+}
